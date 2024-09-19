194aa5470ba1bb7281ed954f41f681b085d83e25
Devyanshu Negi <devyanshunegi@gmail.com>
2024-09-19 08:05:25 +0000

    sign in route

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -4,6 +4,8 @@ import { Message } from "@/model/User";
 export interface ApiResponse {
     success: boolean;
     message: string;
-    isAcceptingMessage?:boolean;
+    isAcceptingMessage?:boolean; // ? for optional
     messages?: Array<Message>;
+
+    // this gives type safety and suggestions
 }
